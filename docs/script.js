e011939b9889a005ebb63cd60592f6f7445a3468
garana123 <jasi.lindern@gmail.com>
2025-03-16 15:35:36 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -551,6 +551,8 @@ function onlyOne(checkbox) {
 document.getElementById("sendenButton").addEventListener("click", () => {
     // 1. Auslesen der "Dienstnummer des Arbeiters" aus dem Element
     const workerInfoText = document.querySelector("dienstnummer").innerText;
+    const workerNumber = workerInfoText || "Unbekannt"; // Fallback-Wert setzen
+
     
 
     // 2. Name aus dem Eingabefeld "kundenname"
