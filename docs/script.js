d2af11ce6441605012cfe9b512ea7176103c85be
kianykn <kianschiki@gmail.com>
2025-04-01 20:27:52 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -261,7 +261,7 @@ document.addEventListener('DOMContentLoaded', () => {
             'tokio manji vip': 'Primärfarbe: Pures Gold <br> Sekundär: Pures Gold <br> Perleffekt: Renngelb <br> Reifenqualm: ./. <br> Unterboden: ./.',
             'blancos vip': 'Primärfarbe: Marmor Braun <br> Sekundärfarbe: Marmor Braun <br> Perleffekt: Creme Weiß <br> Unterboden: Light Blue <br> Headlights: Hellblau',
             'blancos ug vip': 'Primärfarbe: Asphalt Grau <br> Sekundärfarbe: Asphalt Grau <br> Perleffekt: Cobalt Blau', 
-            'sacra corona unita': 'Primärfarbe: Matt Schwarz <br> Sekundärfarbe: Matt Schwarz <br> Perleffekt: Metallic Lila <br> Unterboden: Weiß',
+            'sacra corona unita vip': 'Primärfarbe: Matt Schwarz <br> Sekundärfarbe: Matt Schwarz <br> Perleffekt: Metallic Lila <br> Unterboden: Weiß',
             'alex lang vip2': 'Info: Bekommen 40% auf alle Fahrzeuge',
             'emma nackendick vip2': 'Info: Bekommen 40% auf alle Fahrzeuge', 
             'emir pearce vip2': 'Info: Bekommen 40% auf alle Fahrzeuge',
