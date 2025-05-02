66f9188708d023fae3cdb1893da72a461589d20b
kianykn <kianschiki@gmail.com>
2025-05-02 22:47:52 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -272,7 +272,7 @@ document.addEventListener('DOMContentLoaded', () => {
             'sarah tonic vip2': 'Info: Bekommen 40% auf alle Fahrzeuge', 
             'vargo azrail vip2': 'Info: Bekommen 40% auf alle Fahrzeuge', 
             'kylo sanymaaa vip2': 'Info: Bekommen 40% auf alle Fahrzeuge', 
-            'kean diaz vip2': 'Info: Bekommen 40% auf alle Fahrzeuge',
+            'emreh can vip2': 'Info: Bekommen 40% auf alle Fahrzeuge',
             'abderahim vip2': 'Info: Gilt für alle mit dem Nachnamen Abderahim <br> Bekommen 40% auf alle Fahrzeuge',
             'pd cid vip': 'Primärfarbe: Mitternachtblau <br> Sekundärfarbe: ./. <br> Perleffekt: Stahlguss (Bei Schwarz) <br> Info: Die Ränge Praktikant, Rekrut, Junior Officer und Officer 1 tunen Dienstfahrzeuge kostenlos,', 
             'pd gtf vip': 'Primärfarbe: Schwarz <br> Sekundärfarbe: Schwarz <br> Perleffekt: Schwarz <br> Info: Die Ränge Praktikant, Rekrut, Junior Officer und Officer 1 tunen Dienstfahrzeuge kostenlos',
