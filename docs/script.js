ad58660ddeb73af1ac8752939d45a39bf34b1491
garana123 <jasi.lindern@gmail.com>
2026-04-22 16:42:40 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -223,7 +223,7 @@
             '069': 'Primär: Verbotenes Blau <br> Sekundär: <br> Perleffekt: Mitternachtsblau',
             '666': 'Primär: Matt Grau <br> Sekundär: <br> Perleffekt: Elektrisches Rosa',
             'district 67': 'Primär: Paradies Blau <br> Sekundär: <br> Perleffekt: Matt Schwarz',
-            'k-town': 'Primär: <br> Sekundär: <br> Perleffekt: ',
+            'k-town': 'Primär: Matt Schwarz <br> Sekundär: <br> Perleffekt: Hellgrün ',
             'zivi vip2': 'Info: Bekommen 40% auf alle Fahrzeuge'
 
             
